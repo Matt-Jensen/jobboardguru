31b78e78db21afacb438ceb5c7a33531b49e69ff
Matt Jensen <matt@industriousapps.com>
2023-10-24 18:34:00 -0500

    fix main site url
    Signed-off-by: Matt Jensen <matt@industriousapps.com>

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const MAIN_SITE='https://jobboardguru.com'
+export const MAIN_SITE='https://www.jobboardguru.com'
 export const QUESTIONNAIRE_PATH='/questionnaire'
 export const HOME_OG_IMAGE_URL = 'https://todo.com/'
